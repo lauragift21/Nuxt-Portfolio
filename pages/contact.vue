efcd258a11cb259178ed2178a49a92e084f939cb
egwuenu gift <laurandidi21@gmail.com>
2017-11-04 08:33:01 +0100

    fixed broken link

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,7 +2,7 @@
 	<section class="section is-meduim">
 		<div class="container has-text-centered">
 			<h1 class="title">Looking for me?</h1>
-			<p>You can find me on Twitter  <a href="twitter.com/lauragift21" target="_blank">@lauragift21</a></p>
+			<p>You can find me on Twitter  <a href="https://twitter.com/lauragift21" target="_blank">@lauragift21</a></p>
 		</div>
 	</section>
 </template>
--- pages/projects.vue
+++ pages/projects.vue
@@ -2,7 +2,7 @@
 	<section class="section is-medium">
 		<div class="container has-text-centered">
 			<h1 class="title">My Projects</h1>
-			<p>Most projects hi have worked on can be found on <a href="github.com/lauragift21" target="_blank">Github</a></p>
+			<p>Most projects hi have worked on can be found on <a href="https://github.com/lauragift21" target="_blank">Github</a></p>
 		</div>
 	</section>
 </template>
