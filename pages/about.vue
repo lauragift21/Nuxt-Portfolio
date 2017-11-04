1af34a081022e404d5d1974268dad4b3e65b2ccb
egwuenu gift <laurandidi21@gmail.com>
2017-11-04 08:28:53 +0100

    added emoji :sparkles:

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
 	<section class="section is-medium">
 		<div class="container has-text-centered">
 			<h1 class="title">About Me</h1>
-			<p>I'm Gift lover of coffee and coder, Slayer of all Dragons</p>
+			<p>I'm Gift lover of coffee and coder ☕☕ 👩‍💻, Slayer of all Dragons🔥🔥🔥</p>
 		</div>
 	</section>
 </template>
